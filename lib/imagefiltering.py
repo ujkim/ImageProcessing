54cd26fe662a950b6fe1285771f3a2d7145abef3
DESKTOP-167H3ET\dmlwn <dmlwnd100@naver.com>
2019-01-21 21:52:32 +0900

    filter 추가 2차 스터디

--- lib/imagefiltering.py
+++ lib/imagefiltering.py
@@ -1,4 +1,8 @@
+import numpy as np
 import scipy.ndimage
+from skimage import filters
+from skimage import feature
+import matplotlib.pyplot as plt
 
 class ImageFiltering:
     def __init__(self):
@@ -15,3 +19,28 @@ class ImageFiltering:
 
     def median_filtering(self, image_object, filter_size):
         return scipy.misc.toimage(scipy.ndimage.filters.median_filter(image_object, filter_size))
+
+    def sobel_filtering(self, image_object):
+        return scipy.misc.toimage(filters.sobel(image_object))
+
+    def prewitt_filtering(self, image_object):
+        return scipy.misc.toimage(filters.prewitt(image_object))
+
+    def horizontal_prewitt_filtering(self, image_object):
+        return scipy.misc.toimage(filters.prewitt_h(image_object))
+    
+    def vertical_prewitt_filtering(self, image_object):
+        return scipy.misc.toimage(filters.prewitt_v(image_object))
+
+    def laplace_filtering(self, image_object):
+        return scipy.misc.toimage(scipy.ndimage.filters.laplace(image_object))
+
+    def gaussian_filtering(self, image_object, sigma):
+        return scipy.misc.toimage(scipy.ndimage.filters.gaussian_filter(image_object, sigma))
+
+    def gaussian_laplace_filtering(self, image_object, sigma):
+        return scipy.misc.toimage(scipy.ndimage.filters.gaussian_laplace(image_object, sigma))
+
+    def canny_filtering(self, image_object, sigma):
+        return scipy.misc.toimage(feature.canny(scipy.misc.fromimage(image_object), sigma).astype(np.float32))
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from lib.imagemgr import ImageManager as IMGR
 from lib.imagefilter import ImageFilter as IMGF
 from lib.imagefiltering import ImageFiltering as IMGFL
 
+
 # Execution of Our Image Processing Library
 def main():
     input_filename = "data/lena.png"
@@ -20,12 +21,45 @@ def main():
     # image_obj = imagefiltering.max_filtering(imagemgr.read_image(input_filename), 5)
 
     # minimum filter
-    output_filename = "data/minimum_lena_10x10.png"
-    image_obj = imagefiltering.min_filtering(imagemgr.read_image(input_filename), 10)
+    # output_filename = "data/minimum_lena_10x10.png"
+    # image_obj = imagefiltering.min_filtering(imagemgr.read_image(input_filename), 10)
 
     # mean filter
     # output_filename = "mean_lena_zero.png"
     # image_obj = imagefiltering.mean_filtering(imagemgr.read_image(input_filename), imagefilter.mean_filter(5, 5))
+    
+    # 2019-01-21 Python Image Processing Study
+    # sobel filter
+    # output_filename = "data/sobel_lena.png"
+    # image_obj = imagefiltering.sobel_filtering(imagemgr.read_image(input_filename))
+
+    # prewitt filter
+    # output_filename = "data/prewitt_lena.png"
+    # image_obj = imagefiltering.prewitt_filtering(imagemgr.read_image(input_filename))
+
+    # horizontal prewitt filter
+    # output_filename = "data/horizontal_prewitt_lena.png"
+    # image_obj = imagefiltering.horizontal_prewitt_filtering(imagemgr.read_image(input_filename))
+
+    # vertical prewitt filter
+    # output_filename = "data/vertical_prewitt_lena.png"
+    # image_obj = imagefiltering.vertical_prewitt_filtering(imagemgr.read_image(input_filename))
+
+    # canny filter
+    # output_filename = "data/canny_3.0_lena.png"
+    # image_obj = imagefiltering.canny_filtering(imagemgr.read_image(input_filename), sigma=3.0)
+    
+    # gaussian filter
+    output_filename = "data/gaussian_3.0_lena.png"
+    image_obj = imagefiltering.gaussian_filtering(imagemgr.read_image(input_filename), sigma=3.0)
+
+    # Laplace filter
+    # output_filename = "data/laplace_lena.png"
+    # image_obj = imagefiltering.laplace_filtering(imagemgr.read_image(input_filename))
+
+    # Gaussian Laplace filter
+    # output_filename = "data/gaussian_laplace_lena_sigma_3.png"
+    # image_obj = imagefiltering.gaussian_laplace_filtering(imagemgr.read_image(input_filename), sigma=3)
     imagemgr.write_image(image_obj, output_filename)
 
 main()
